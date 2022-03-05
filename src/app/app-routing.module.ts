56fd5cebd3570be012769463aba3bc7d289a8482
mdperini <perinimichaeld@yahoo.com>
2022-03-05 09:02:11 -0500

    intial build-out

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddressBookComponent } from './components/address-book/address-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AddressBookComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressBookComponent } from './components/address-book/address-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressBookComponent
   ],
   imports: [
     BrowserModule,
