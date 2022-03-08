1b69fa911a2d46d11922d11f4f937fb485dfde08
mdperini <perinimichaeld@yahoo.com>
2022-03-08 07:13:04 -0500

    Update address-book-detail.component.spec.ts

--- src/app/components/address-book-detail/address-book-detail.component.spec.ts
+++ src/app/components/address-book-detail/address-book-detail.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { addressBookData } from 'src/app/shared/data/address-book-data';
+import { emptyAddressBookEntry } from 'src/app/shared/data/empty-address-book-entry';
 
 import { AddressBookDetailComponent } from './address-book-detail.component';
 
@@ -21,5 +23,16 @@ describe('AddressBookDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('initially should equal', () => {
+    expect(component.addressBookEntry).toEqual(emptyAddressBookEntry);
+  });
+
+  it('on clear should equal', () => {
+    component.addressBookEntry = addressBookData[0]
+    expect(component.addressBookEntry).toEqual(addressBookData[0]);
+    component.onClear();
+    expect(component.addressBookEntry).toEqual(emptyAddressBookEntry);
   });
 });
